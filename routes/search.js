c5d5589bc7d510764d0db95304e9faf6444bcd4a
Tim <tim.lindi@outlook.de>
2020-06-14 13:56:38 +0200

    updated search route

--- routes/search.js
+++ routes/search.js
@@ -3,26 +3,32 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const _= require('lodash');
-var NewSpotifyApi=require('../Classes/newSpotifyApi')
 var express = require('express');
 var router = express.Router();
 const { User } = require('../models/user');
 const {Party}= require('../models/party')
 
+//Create Instance ot Spotify API
+var NewSpotifyApi=require('../Classes/newSpotifyApi')
 var newSpotifyApi= new NewSpotifyApi(
     process.env.client_Id,
     process.env.client_secret_key,
     process.env.redirect_uri
 )
 
+//Search for Songs and return Songs
+//Returns list of 10 Songs in the Format {artist, songId, title, albumArt}
 router.post('/', async(req,res)=>{
     try{
         console.log(req.body)
         var party = await Party.findById(req.body.partyId)
         var user=await User.findById(party.user);
+        if(!party || !user) return res.status(400).send("Party or User not found")
+        
         var accessToken=await user.isAccessValid();
         var search = await newSpotifyApi.search(accessToken, req.body.query);
-        //Format Result
+        
+        //Format the Results
         var tracks=search.tracks.items;
         var result=[]
         tracks.forEach((item)=>{result.push({
@@ -34,8 +40,9 @@ router.post('/', async(req,res)=>{
         res.send(result);
         }
         catch (err){
+            console.log("Error in Endpoint Post Search:")
             console.log(err.message);
-            res.status(400).send("Error Finding title")
+            res.status(400).send(err.message)
         }
 });
 
