80cecd21f31dff536434a5b06bc4ae09659150c0
Tim <tim.lindi@outlook.de>
2020-06-14 13:56:47 +0200

    updated user route

--- routes/user.js
+++ routes/user.js
@@ -2,21 +2,28 @@ const _= require('lodash');
 const { User } = require('../models/user');
 var express = require('express');
 var router=express.Router();
-const bcrypt=require('bcryptjs')
 
 
-//Creates a new User 
+
+//Create a new User 
 router.post('/createNewUser', async (req, res) => {
-    console.log("Request")
+    //Normaly there is no Reason why this shoud fail -> Try could be unneceessary
+    try{
     let user = new User();
     user = await user.save();
     res.send({id: user._id});
+    }
+    catch(err){
+        console.log("Error in Endpoint post createNewUser")
+        console.log(err.message)
+        res.status(400).send(err.message)
+    }
 
 });
 
-//Validate User
+//Validate if user exitsy by Id
 router.get('/validate', async (req,res)=>{
-    console.log(req.query)
+    try{
     let user = await User.findById(req.query._id)
     if (user){
         res.send(user._id)
@@ -24,6 +31,12 @@ router.get('/validate', async (req,res)=>{
     else {
         res.status(400).send("Error User not found")
     }
+}
+catch(err){
+    console.log("Error in Endpoint get validate")
+    console.log(err.message)
+    res.status(400).send(err.message)
+}
 }
 )
 
